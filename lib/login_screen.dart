fcffda9851f85c059b01124f6b2bcc3f8b1cbf7d
Mohd Anas <mohdanas.dev70@gmail.com>
2024-02-29 15:35:52 +0530

    Login Screen UI Done

---
+++ lib/login_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:zoom_full_stack/widgets/custom_button.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          const Text(
+            'Start or join a meeting',
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 38.0),
+            child: Image.asset('assets/images/onboarding.jpg'),
+          ),
+          CustomButton(
+            text: 'Google Sign In',
+            onPressed: () {},
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widgets/custom_button.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:zoom_full_stack/utils/colors.dart';
+
+class CustomButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+  const CustomButton({super.key, required this.text,required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(18.0),
+      child: ElevatedButton(
+        onPressed: onPressed,
+        child: Text(
+          text,
+          style: const TextStyle(fontSize: 17),
+        ),
+        style: ElevatedButton.styleFrom(
+          backgroundColor: buttonColor,
+          minimumSize: const Size(double.infinity, 50)
+        ),
+      ),
+    );
+  }
+}
